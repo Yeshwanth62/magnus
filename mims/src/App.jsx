37feca5d8e12b68383633cdabef72d1a7f6e7873
Yeshwanth62 <vishalyeshwanth10@gmail.com>
2025-12-19 00:03:11 +0530

    update frontend

---
+++ mims/src/App.jsx
@@ -0,0 +1,38 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+export default function App() {
+  return (
+    <div className="page">
+      <section className="poster">
+        <img src="/images/magnus-hero.jpeg" alt="Magnus 2.0" />
+      </section>
+
+      <section className="poster">
+        <img src="/images/about-mims.jpeg" alt="About MIMS" />
+      </section>
+
+      <section className="poster">
+        <img src="/images/about-magnus.jpeg" alt="About Magnus" />
+      </section>
+
+      <section className="register">
+        <button
+        onClick={() => window.open("/public/MIMS_Magnus_2.0.pdf", "_blank")}
+  >
+        MIMS MAGNUS 2.0 BROCHURE
+        </button>
+
+        <button>
+          REGISTER NOW
+        </button>
+
+
+      </section>
+
+    
+    </div>
+  );
+}
